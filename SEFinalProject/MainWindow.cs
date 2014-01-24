93623d97c0a96d27ee1c53c5c3d34c44b3b5c78c
Jason Wihardja <jason.wihardja@gmail.com>
2014-01-24 19:06:12 +0700

    Baru bisa connect dan disconnect ke device

---
+++ SEFinalProject/MainWindow.cs
@@ -0,0 +1,52 @@
+﻿using DPCtlUruNet;
+using DPUruNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SEFinalProject {
+    public partial class MainWindow : Form {
+        private Reader fingerprintReader;
+
+
+        public MainWindow() {
+            InitializeComponent();
+            // InitializeVars();
+        }
+
+        public void InitializeVars() {
+            
+        }
+
+        private void MainWindow_Load(Object sender, EventArgs e) {
+            ReaderCollection readers = ReaderCollection.GetReaders();
+            if (readers.Count == 0) {
+                // Fingerprint Reader Not Found
+                MessageBox.Show(this, "Fingerprint Device Not Found!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            } else {
+                // Fingerprint Reader Found
+                fingerprintReader = readers.FirstOrDefault();
+                Constants.ResultCode result = fingerprintReader.Open(Constants.CapturePriority.DP_PRIORITY_COOPERATIVE);
+
+                if (result != Constants.ResultCode.DP_SUCCESS) {
+                    // Error Opening Connection To Device
+                    MessageBox.Show(this, "Error Connecting To Fingerprint Device!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.Close();
+                }
+            }
+        }
+
+        private void MainWindow_FormClosing(object sender, FormClosingEventArgs e) {
+            if (fingerprintReader != null) {
+                fingerprintReader.Dispose();
+            }
+        }
+    }
+}
