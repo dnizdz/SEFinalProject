1ac0dcdccbf76e48ab8c8642dfb16289914f054f
Jason Wihardja <jason.wihardja@gmail.com>
2014-01-24 22:12:55 +0700

    Try to create main UI

--- SEFinalProject/MainWindow.Designer.cs
+++ SEFinalProject/MainWindow.Designer.cs
@@ -23,22 +23,123 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.White;
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(478, 478);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 77F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(484, 561);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 487);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(478, 71);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(66, 7);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(403, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Role";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(66, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(403, 26);
+            this.textBox2.TabIndex = 3;
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(484, 561);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "MainWindow";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Attendance System";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWindow_FormClosing);
             this.Load += new System.EventHandler(this.MainWindow_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- SEFinalProject/MainWindow.cs
+++ SEFinalProject/MainWindow.cs
@@ -5,26 +5,31 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SEFinalProject {
     public partial class MainWindow : Form {
         private Reader fingerprintReader;
-
+        private Thread imageCaptureThread;
 
         public MainWindow() {
             InitializeComponent();
-            // InitializeVars();
+            InitializeVars();
         }
 
         public void InitializeVars() {
-            
+            this.pictureBox.Image = null;
+            this.imageCaptureThread = null;
         }
 
         private void MainWindow_Load(Object sender, EventArgs e) {
+            this.pictureBox.Focus();
+
             ReaderCollection readers = ReaderCollection.GetReaders();
             if (readers.Count == 0) {
                 // Fingerprint Reader Not Found
@@ -38,9 +43,89 @@ namespace SEFinalProject {
                 if (result != Constants.ResultCode.DP_SUCCESS) {
                     // Error Opening Connection To Device
                     MessageBox.Show(this, "Error Connecting To Fingerprint Device!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    fingerprintReader.Dispose();
                     this.Close();
                 }
+
+                imageCaptureThread = new Thread(CaptureThread);
+                imageCaptureThread.IsBackground = true;
+                imageCaptureThread.Start();
             }
+        }
+
+        private void CaptureThread() {
+            while (true) {
+                Fid fid = null;
+
+                if (!CaptureFinger(ref fid)) break;
+                if (fid == null) {
+                    Bitmap bmp = new Bitmap(1, 1);
+                    bmp.SetPixel(0, 0, Color.White);
+                    bmp = new Bitmap(bmp, pictureBox.Width, pictureBox.Height);
+
+                    RefreshImageDelegate rid = new RefreshImageDelegate(RefreshImage);
+                    this.Invoke(rid, new Object[] { null });
+
+                    continue;
+                }
+
+                foreach (Fid.Fiv fiv in fid.Views) {
+                    Bitmap bmp = CreateBitmap(fiv.RawImage, fiv.Width, fiv.Height);
+                    RefreshImageDelegate rid = new RefreshImageDelegate(RefreshImage);
+                    this.Invoke(rid, new Object[] { bmp });
+                }
+            }
+        }
+
+        private delegate void RefreshImageDelegate(Bitmap bmp);
+        private void RefreshImage(Bitmap bmp) {
+            pictureBox.Image = bmp;
+            pictureBox.Refresh();
+        }
+
+        private Boolean CaptureFinger(ref Fid fid) {
+            CaptureResult captureResult = fingerprintReader.Capture(Constants.Formats.Fid.ANSI, Constants.CaptureProcessing.DP_IMG_PROC_DEFAULT, 5000, fingerprintReader.Capabilities.Resolutions.FirstOrDefault());
+            if (captureResult.ResultCode != Constants.ResultCode.DP_SUCCESS) {
+                return false;
+            }
+
+            if (captureResult.Quality == Constants.CaptureQuality.DP_QUALITY_CANCELED) {
+                return false;
+            }
+
+            if ((captureResult.Quality == Constants.CaptureQuality.DP_QUALITY_NO_FINGER || captureResult.Quality == Constants.CaptureQuality.DP_QUALITY_TIMED_OUT)) {
+                return true;
+            }
+
+            if ((captureResult.Quality == Constants.CaptureQuality.DP_QUALITY_FAKE_FINGER)) {
+                return true;
+            }
+
+            fid = captureResult.Data;
+
+            return true;
+        }
+
+        private Bitmap CreateBitmap(Byte[] bytes, Int32 width, Int32 height) {
+            Byte[] rgbBytes = new byte[bytes.Length * 3];
+
+            for (Int32 i = 0; i <= bytes.Length - 1; i++) {
+                rgbBytes[(i * 3)] = bytes[i];
+                rgbBytes[(i * 3) + 1] = bytes[i];
+                rgbBytes[(i * 3) + 2] = bytes[i];
+            }
+            Bitmap bmp = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+
+            BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+
+            for (int i = 0; i <= bmp.Height - 1; i++) {
+                IntPtr p = new IntPtr(data.Scan0.ToInt32() + data.Stride * i);
+                System.Runtime.InteropServices.Marshal.Copy(rgbBytes, i * bmp.Width * 3, p, bmp.Width * 3);
+            }
+
+            bmp.UnlockBits(data);
+
+            return bmp;
         }
 
         private void MainWindow_FormClosing(object sender, FormClosingEventArgs e) {
