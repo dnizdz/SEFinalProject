6739da4f95e50a497877bbf41dc956c8113b1982
Jason Wihardja <jason.wihardja@gmail.com>
2014-01-28 00:01:13 +0700

    Done (Haven't Bug Check)

--- SEFinalProject/DataDeletionForm.Designer.cs
+++ SEFinalProject/DataDeletionForm.Designer.cs
@@ -24,20 +24,77 @@
         /// </summary>
         private void InitializeComponent() {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DataDeletionForm));
+            this.idLabel = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.deleteBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idLabel.Location = new System.Drawing.Point(12, 15);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(100, 20);
+            this.idLabel.TabIndex = 0;
+            this.idLabel.Text = "Employee ID";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idTextBox.Location = new System.Drawing.Point(118, 12);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(575, 26);
+            this.idTextBox.TabIndex = 1;
+            this.idTextBox.TextChanged += new System.EventHandler(this.idTextBox_TextChanged);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(11, 50);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(682, 150);
+            this.nameLabel.TabIndex = 2;
+            this.nameLabel.Text = "Name";
+            this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteBtn.Location = new System.Drawing.Point(12, 203);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(681, 30);
+            this.deleteBtn.TabIndex = 3;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
             // 
             // DataDeletionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(705, 245);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.idLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "DataDeletionForm";
-            this.Text = "DataDeletionForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Data Deletion Form";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Button deleteBtn;
     }
 }
--- SEFinalProject/DataDeletionForm.cs
+++ SEFinalProject/DataDeletionForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,15 +12,68 @@ using System.Windows.Forms;
 namespace SEFinalProject {
     public partial class DataDeletionForm : Form {
         private MainWindow mainWindow;
+        private Boolean isFound;
 
         public DataDeletionForm(MainWindow mainWindow) {
             InitializeComponent();
             this.Activated += new System.EventHandler(this.DataDeletionForm_Activated);
             this.mainWindow = mainWindow;
+            this.isFound = false;
+            this.nameLabel.Text = String.Empty;
         }
 
         private void DataDeletionForm_Activated(object sender, EventArgs e) {
-            
+            this.idTextBox.Focus();
+        }
+
+        private void idTextBox_TextChanged(object sender, EventArgs e) {
+            this.isFound = false;
+            this.nameLabel.Text = String.Empty;
+
+            try {
+                MySqlConnection con = new MySqlConnection("SERVER=localhost;DATABASE=se;UID=root;PASSWORD=;");
+                con.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "SELECT * FROM employee WHERE empID = \'" + this.idTextBox.Text + "\'";
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read()) {
+                    this.nameLabel.Text = reader.GetString("name");
+                }
+
+                reader.Close();
+                con.Close();
+
+                this.isFound = true;
+            } catch (MySqlException) {
+                MessageBox.Show(this, "Error Retrieving Data From Database!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                mainWindow.Close();
+            }
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e) {
+            if (isFound) {
+                try {
+                    MySqlConnection con = new MySqlConnection("SERVER=localhost;DATABASE=se;UID=root;PASSWORD=;");
+                    con.Open();
+
+                    MySqlCommand cmd = new MySqlCommand();
+                    cmd.Connection = con;
+                    cmd.CommandText = "DELETE FROM employee WHERE empID = \'" + this.idTextBox.Text + "\'";
+
+                    cmd.ExecuteNonQuery();
+
+                    con.Close();
+
+                    this.idTextBox.Text = String.Empty;
+                    this.nameLabel.Text = "Data Successfully Deleted.";
+                } catch (MySqlException) {
+                    MessageBox.Show(this, "Error Deleting Data From Database!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    mainWindow.Close();
+                }
+            }
         }
     }
 }
